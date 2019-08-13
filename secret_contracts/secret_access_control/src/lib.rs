5edb8c07c30ecbbdc815defbf653d25f99551029
Guillaume Duveau <guillaume.duveau@gmail.com>
2019-08-13 17:46:14 +0200

    emulate access control, codestyle, comments

--- secret_contracts/secret_access_control/src/lib.rs
+++ secret_contracts/secret_access_control/src/lib.rs
@@ -1,76 +1,85 @@
-// We need chars() which is in str::str
+// Built-In Attributes.
 #![no_std]
 
-// Imports
+// Imports.
 extern crate eng_wasm;
 extern crate eng_wasm_derive;
 extern crate serde;
 
+// Uses.
 use eng_wasm::*;
 use eng_wasm_derive::pub_interface;
 use serde::{Serialize, Deserialize};
 
-// Encrypted state keys
+// Encrypted state keys.
 static SECRET_MESSAGES: &str = "secretMessages";
 
-// Structs
+// Structs.
 #[derive(Serialize, Deserialize)]
 pub struct SecretMessage {
     whitelist: Vec<H160>,
     secret_message: String,
 }
 
-// Public struct Contract which will consist of private and public-facing secret contract functions
+// Public struct Contract which will consist of private and public-facing secret contract functions.
 pub struct Contract;
 
-// Private functions accessible only by the secret contract
+// Private functions accessible only by the secret contract.
 impl Contract {
-    fn get_secret_messages() -> Vec<SecretMessage> {
+    // Get all secret messages.
+    fn get_all_secret_messages() -> Vec<SecretMessage> {
         read_state!(SECRET_MESSAGES).unwrap_or_default()
     }
 }
 
-// Public trait defining public-facing secret contract functions
+// Public trait defining public-facing secret contract functions.
 #[pub_interface]
 pub trait ContractInterface {
+    // Send a secret messages to recipients.
     fn send_secret_message(addresses: Vec<H160>, message: String);
-    fn read_messages() -> String;
+
+    // Read messages for a user (sender).
+    fn read_messages(sender: H160) -> String;
 }
 
 // Implementation of the public-facing secret contract functions defined in the ContractInterface
-// trait implementation for the Contract struct above
+// trait implementation for the Contract struct above.
 impl ContractInterface for Contract {
     #[no_mangle]
     fn send_secret_message(addresses: Vec<H160>, message: String) {
-        let mut secret_messages = Self::get_secret_messages();
+        // Get all secret messages.
+        let mut secret_messages = Self::get_all_secret_messages();
+        // Push new secret message.
         secret_messages.push(SecretMessage {
             whitelist: addresses,
             secret_message: message,
         });
+        // Write state.
         write_state!(SECRET_MESSAGES => secret_messages);
     }
 
     #[no_mangle]
-    fn read_messages() -> String {
+    fn read_messages(sender: H160) -> String {
         // We can not return an array of strings, so we'll return a concatenated string of results.
         let mut my_concatenated_secret_messages: String = String::new();
         // Separator between strings.
         let separator = String::from("|");
         // Get all secret messages.
-        let all_secret_messages = Self::get_secret_messages();
+        let all_secret_messages = Self::get_all_secret_messages();
         // Loop through them.
-        for x in all_secret_messages {
-            // We would need Solidity's msg.sender to filter for whitelisted senders.
-            // Externally it's called with computeTask(fn, args, gasLimit, gasPx, sender, scAddr)
-            // Is there a way to get sender here?
-
-            // Concatenate the message with the others.
-            my_concatenated_secret_messages.push_str(&x.secret_message);
-            // Add the separator.
-            my_concatenated_secret_messages.push_str(&separator);
+        for one_secret_message in all_secret_messages {
+            // For a given message, is the user (sender) in the whitelist?
+            for recipient in one_secret_message.whitelist {
+                // Yes, so add the message.
+                if recipient == sender {
+                    // Concatenate the message with the others.
+                    my_concatenated_secret_messages.push_str(&one_secret_message.secret_message);
+                    // Add the separator.
+                    my_concatenated_secret_messages.push_str(&separator);
+                }
+            }
         }
-        // Remove last separator.
+        // Remove last separator & return messages.
         my_concatenated_secret_messages.pop();
-        return my_concatenated_secret_messages;
     }
 }
--- test/test_secret_access_control.js
+++ test/test_secret_access_control.js
@@ -1,26 +1,25 @@
-const fs = require('fs');
-const path = require('path');
-const dotenv = require('dotenv');
-const { Enigma, utils, eeConstants } = require('enigma-js/node');
+const fs = require('fs')
+const path = require('path')
+const dotenv = require('dotenv')
+const { Enigma, utils, eeConstants } = require('enigma-js/node')
 
-var EnigmaContract;
+var EnigmaContract
 if (typeof process.env.SGX_MODE === 'undefined' || (process.env.SGX_MODE != 'SW' && process.env.SGX_MODE != 'HW')) {
-  console.log(`Error reading ".env" file, aborting....`);
-  process.exit();
+  console.log(`Error reading ".env" file, aborting....`)
+  process.exit()
 } else if (process.env.SGX_MODE == 'SW') {
-  EnigmaContract = require('../build/enigma_contracts/EnigmaSimulation.json');
+  EnigmaContract = require('../build/enigma_contracts/EnigmaSimulation.json')
 } else {
-  EnigmaContract = require('../build/enigma_contracts/Enigma.json');
+  EnigmaContract = require('../build/enigma_contracts/Enigma.json')
 }
-const EnigmaTokenContract = require('../build/enigma_contracts/EnigmaToken.json');
+const EnigmaTokenContract = require('../build/enigma_contracts/EnigmaToken.json')
 
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 const splitMessages = decryptedOutput => {
-  const separator = '|'
   const decodedParameters = web3.eth.abi.decodeParameters(
     [
       {
@@ -30,13 +29,20 @@ const splitMessages = decryptedOutput => {
     ],
     decryptedOutput
   )
+  const concatenatedMessages = decodedParameters.concatenatedMessages
+  // Return empty array of messages if decrypted output string is empty.
+  if (concatenatedMessages === '') {
+    return []
+  }
+  // Otherwise return messages.
+  const separator = '|'
   return decodedParameters.concatenatedMessages.split(separator)
 }
 
-let enigma = null;
-let contractAddr;
+let enigma = null
+let contractAddr
 
-contract("SecretAccessControl", accounts => {
+contract('SecretAccessControl', accounts => {
   const alice = accounts[0]
   const bob = accounts[1]
   const charles = accounts[2]
@@ -45,7 +51,7 @@ contract("SecretAccessControl", accounts => {
   const taskGasLimit = 10000000
   const taskGasPx = utils.toGrains(1e-7)
 
-  before(function () {
+  before(() => {
     enigma = new Enigma(
       web3,
       EnigmaContract.networks['4447'].address,
@@ -56,63 +62,64 @@ contract("SecretAccessControl", accounts => {
         gasPrice: 100000000000,
         from: alice,
       },
-    );
-    enigma.admin();
-
-    contractAddr = fs.readFileSync('test/secret_access_control.txt', 'utf-8');
-  });
-
-  let task;
+    )
+    enigma.admin()
+    contractAddr = fs.readFileSync('test/secret_access_control.txt', 'utf-8')
+  })
 
   // Alice sends a secret message to Bob and Charles.
   it('Alice should send a secret message to Bob and Charles', async () => {
-    let taskFn = 'send_secret_message(address[],string)';
-    let taskArgs = [
+    let task
+    // Enigma secret contract function to call in the compute task.
+    const taskFn = 'send_secret_message(address[],string)'
+    // Function parameters.
+    const taskArgs = [
       [[bob, charles], 'address[]'],
       ["Hi Bob and Charles!", 'string'],
-    ];
+    ]
+    // Compute task.
     task = await new Promise((resolve, reject) => {
       enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, alice, contractAddr)
         .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-        .on(eeConstants.ERROR, (error) => reject(error));
-    });
+        .on(eeConstants.ERROR, (error) => reject(error))
+    })
     // Task should be pending.
-    task = await enigma.getTaskRecordStatus(task);
-    expect(task.ethStatus).to.equal(1);
+    task = await enigma.getTaskRecordStatus(task)
+    expect(task.ethStatus).to.equal(1)
     // Task should be completed after a while.
     do {
-      await sleep(1000);
-      task = await enigma.getTaskRecordStatus(task);
-    } while (task.ethStatus === 1);
-    expect(task.ethStatus).to.equal(2);
-  });
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
+  })
 
   // Bob reads his messages.
   it('Bob should have 1 message from Alice', async () => {
-    let taskFn = 'read_messages()';
-    let taskArgs = [];
+    let task
+    const taskFn = 'read_messages()'
+    const taskArgs = [
+      [bob, 'address']
+    ]
     task = await new Promise((resolve, reject) => {
       enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, bob, contractAddr)
         .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-        .on(eeConstants.ERROR, (error) => reject(error));
-    });
-    // Task should be pending.
-    task = await enigma.getTaskRecordStatus(task);
-    expect(task.ethStatus).to.equal(1);
-    // Task should be completed after a while.
+        .on(eeConstants.ERROR, (error) => reject(error))
+    })
     do {
-      await sleep(1000);
-      task = await enigma.getTaskRecordStatus(task);
-    } while (task.ethStatus === 1);
-    expect(task.ethStatus).to.equal(2);
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
     // Bob should get 1 message from Alice.
     task = await new Promise((resolve, reject) => {
       enigma.getTaskResult(task)
         .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
-        .on(eeConstants.ERROR, (error) => reject(error));
-    });
-    expect(task.engStatus).to.equal('SUCCESS');
-    task = await enigma.decryptTaskResult(task);
+        .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    expect(task.engStatus).to.equal('SUCCESS')
+    task = await enigma.decryptTaskResult(task)
+    // Rebuild messages from concatenated string of task decrypted output.
     const messages = splitMessages(task.decryptedOutput)
     // deep.equal instead of equal
     // @see https://medium.com/@victorleungtw/testing-with-mocha-array-comparison-e9a45b57df27
@@ -121,57 +128,57 @@ contract("SecretAccessControl", accounts => {
         'Hi Bob and Charles!'
       ]
     )
-  });
+  })
 
   // Alice sends another secret message to Bob and Dave.
   it('Alice should send a secret message to Bob and Dave', async () => {
-    let taskFn = 'send_secret_message(address[],string)';
-    let taskArgs = [
+    let task
+    const taskFn = 'send_secret_message(address[],string)'
+    const taskArgs = [
       [[bob, dave], 'address[]'],
       ["Hi Bob and Dave!", 'string'],
-    ];
+    ]
     task = await new Promise((resolve, reject) => {
       enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, alice, contractAddr)
         .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-        .on(eeConstants.ERROR, (error) => reject(error));
-    });
-    // Task should be pending.
-    task = await enigma.getTaskRecordStatus(task);
-    expect(task.ethStatus).to.equal(1);
-    // Task should be completed after a while.
+        .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    task = await enigma.getTaskRecordStatus(task)
+    expect(task.ethStatus).to.equal(1)
     do {
-      await sleep(1000);
-      task = await enigma.getTaskRecordStatus(task);
-    } while (task.ethStatus === 1);
-    expect(task.ethStatus).to.equal(2);
-  });
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
+  })
 
   // Bob reads his messages again.
   it('Bob should have 2 messages from Alice', async () => {
-    let taskFn = 'read_messages()';
-    let taskArgs = [];
+    let task
+    const taskFn = 'read_messages()'
+    const taskArgs = [
+      [bob, 'address']
+    ]
     task = await new Promise((resolve, reject) => {
         enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, bob, contractAddr)
             .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-            .on(eeConstants.ERROR, (error) => reject(error));
-    });
-    // Task should be pending.
-    task = await enigma.getTaskRecordStatus(task);
-    expect(task.ethStatus).to.equal(1);
-    // Task should be completed after a while.
+            .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    task = await enigma.getTaskRecordStatus(task)
+    expect(task.ethStatus).to.equal(1)
     do {
-      await sleep(1000);
-      task = await enigma.getTaskRecordStatus(task);
-    } while (task.ethStatus === 1);
-    expect(task.ethStatus).to.equal(2);
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
     // Bob should get 2 messages from Alice.
     task = await new Promise((resolve, reject) => {
     enigma.getTaskResult(task)
       .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
-      .on(eeConstants.ERROR, (error) => reject(error));
-    });
-    expect(task.engStatus).to.equal('SUCCESS');
-    task = await enigma.decryptTaskResult(task);
+      .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    expect(task.engStatus).to.equal('SUCCESS')
+    task = await enigma.decryptTaskResult(task)
     const messages = splitMessages(task.decryptedOutput)
     expect(messages).to.deep.equal(
       [
@@ -179,117 +186,107 @@ contract("SecretAccessControl", accounts => {
         'Hi Bob and Dave!'
       ]
     )
-  });
+  })
 
-  // // Charles reads his messages.
-  // it('Charles should have 1 message from Alice', async () => {
-  //   let taskFn = 'read_messages()';
-  //   let taskArgs = [];
-  //   task = await new Promise((resolve, reject) => {
-  //       enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, charles, contractAddr)
-  //           .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-  //           .on(eeConstants.ERROR, (error) => reject(error));
-  //   });
-  //   // Task should be pending.
-  //   task = await enigma.getTaskRecordStatus(task);
-  //   expect(task.ethStatus).to.equal(1);
-  //   // Task should be completed after a while.
-  //   do {
-  //     await sleep(1000);
-  //     task = await enigma.getTaskRecordStatus(task);
-  //   } while (task.ethStatus === 1);
-  //   expect(task.ethStatus).to.equal(2);
-  //   // Charles should get 1 message from Alice.
-  //   task = await new Promise((resolve, reject) => {
-  //   enigma.getTaskResult(task)
-  //     .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
-  //     .on(eeConstants.ERROR, (error) => reject(error));
-  //   });
-  //   expect(task.engStatus).to.equal('SUCCESS');
-  //   task = await enigma.decryptTaskResult(task);
-  //   // deep.equal instead of equal
-  //   // @see https://medium.com/@victorleungtw/testing-with-mocha-array-comparison-e9a45b57df27
-  //   expect(web3.eth.abi.decodeParameters([{
-  //       type: 'string[]',
-  //       name: 'messages',
-  //   }], task.decryptedOutput).messages)
-  //   .to.deep.equal([
-  //     'Hi Bob and Charles!'
-  //     ]
-  //   );
-  // });
+  // Charles reads his messages.
+  it('Charles should have 1 message from Alice', async () => {
+    let task
+    const taskFn = 'read_messages()'
+    const taskArgs = [
+      [charles, 'address']
+    ]
+    task = await new Promise((resolve, reject) => {
+        enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, charles, contractAddr)
+            .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+            .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    task = await enigma.getTaskRecordStatus(task)
+    expect(task.ethStatus).to.equal(1)
+    do {
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
+    // Charles should get 1 message from Alice.
+    task = await new Promise((resolve, reject) => {
+    enigma.getTaskResult(task)
+      .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+      .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    expect(task.engStatus).to.equal('SUCCESS')
+    task = await enigma.decryptTaskResult(task)
+    const messages = splitMessages(task.decryptedOutput)
+    expect(messages).to.deep.equal(
+      [
+        'Hi Bob and Charles!'
+      ]
+    )
+  })
 
-  // // Dave reads his messages.
-  // it('Dave should have 1 message from Alice', async () => {
-  //   let taskFn = 'read_messages()';
-  //   let taskArgs = [];
-  //   task = await new Promise((resolve, reject) => {
-  //       enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, dave, contractAddr)
-  //           .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-  //           .on(eeConstants.ERROR, (error) => reject(error));
-  //   });
-  //   // Task should be pending.
-  //   task = await enigma.getTaskRecordStatus(task);
-  //   expect(task.ethStatus).to.equal(1);
-  //   // Task should be completed after a while.
-  //   do {
-  //     await sleep(1000);
-  //     task = await enigma.getTaskRecordStatus(task);
-  //   } while (task.ethStatus === 1);
-  //   expect(task.ethStatus).to.equal(2);
-  //   // Dave should get 1 message from Alice.
-  //   task = await new Promise((resolve, reject) => {
-  //   enigma.getTaskResult(task)
-  //     .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
-  //     .on(eeConstants.ERROR, (error) => reject(error));
-  //   });
-  //   expect(task.engStatus).to.equal('SUCCESS');
-  //   task = await enigma.decryptTaskResult(task);
-  //   // deep.equal instead of equal
-  //   // @see https://medium.com/@victorleungtw/testing-with-mocha-array-comparison-e9a45b57df27
-  //   expect(web3.eth.abi.decodeParameters([{
-  //       type: 'string[]',
-  //       name: 'messages',
-  //   }], task.decryptedOutput).messages)
-  //   .to.deep.equal([
-  //     'Hi Bob and Dave!'
-  //     ]
-  //   );
-  // });
+  // Dave reads his messages.
+  it('Dave should have 1 message from Alice', async () => {
+    let task
+    const taskFn = 'read_messages()'
+    const taskArgs = [
+      [dave, 'address']
+    ]
+    task = await new Promise((resolve, reject) => {
+        enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, dave, contractAddr)
+            .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+            .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    task = await enigma.getTaskRecordStatus(task)
+    expect(task.ethStatus).to.equal(1)
+    do {
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
+    // Dave should get 1 message from Alice.
+    task = await new Promise((resolve, reject) => {
+    enigma.getTaskResult(task)
+      .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+      .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    expect(task.engStatus).to.equal('SUCCESS')
+    task = await enigma.decryptTaskResult(task)
+    const messages = splitMessages(task.decryptedOutput)
+    expect(messages).to.deep.equal(
+      [
+        'Hi Bob and Dave!'
+      ]
+    )
+  })
 
-  // // Eve reads her messages.
-  // it('Eve should have no message from Alice', async () => {
-  //   let taskFn = 'read_messages()';
-  //   let taskArgs = [];
-  //   task = await new Promise((resolve, reject) => {
-  //       enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, eve, contractAddr)
-  //           .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
-  //           .on(eeConstants.ERROR, (error) => reject(error));
-  //   });
-  //   // Task should be pending.
-  //   task = await enigma.getTaskRecordStatus(task);
-  //   expect(task.ethStatus).to.equal(1);
-  //   // Task should be completed after a while.
-  //   do {
-  //     await sleep(1000);
-  //     task = await enigma.getTaskRecordStatus(task);
-  //   } while (task.ethStatus === 1);
-  //   expect(task.ethStatus).to.equal(2);
-  //   // Eve should get no message from Alice.
-  //   task = await new Promise((resolve, reject) => {
-  //   enigma.getTaskResult(task)
-  //     .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
-  //     .on(eeConstants.ERROR, (error) => reject(error));
-  //   });
-  //   expect(task.engStatus).to.equal('SUCCESS');
-  //   task = await enigma.decryptTaskResult(task);
-  //   // deep.equal instead of equal
-  //   // @see https://medium.com/@victorleungtw/testing-with-mocha-array-comparison-e9a45b57df27
-  //   expect(web3.eth.abi.decodeParameters([{
-  //       type: 'string[]',
-  //       name: 'messages',
-  //   }], task.decryptedOutput).messages)
-  //   .to.deep.equal([]);
-  // });
+  // Eve reads her messages.
+  it('Eve should have no message from Alice', async () => {
+    let task
+    const taskFn = 'read_messages()'
+    const taskArgs = [
+      [eve, 'address']
+    ]
+    task = await new Promise((resolve, reject) => {
+        enigma.computeTask(taskFn, taskArgs, taskGasLimit, taskGasPx, eve, contractAddr)
+            .on(eeConstants.SEND_TASK_INPUT_RESULT, (result) => resolve(result))
+            .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    task = await enigma.getTaskRecordStatus(task)
+    expect(task.ethStatus).to.equal(1)
+    do {
+      await sleep(1000)
+      task = await enigma.getTaskRecordStatus(task)
+    } while (task.ethStatus === 1)
+    expect(task.ethStatus).to.equal(2)
+    // Eve should get no message from Alice.
+    task = await new Promise((resolve, reject) => {
+    enigma.getTaskResult(task)
+      .on(eeConstants.GET_TASK_RESULT_RESULT, (result) => resolve(result))
+      .on(eeConstants.ERROR, (error) => reject(error))
+    })
+    expect(task.engStatus).to.equal('SUCCESS')
+    task = await enigma.decryptTaskResult(task)
+    const messages = splitMessages(task.decryptedOutput)
+    expect(messages).to.deep.equal([])
+  })
 
-});
+})
